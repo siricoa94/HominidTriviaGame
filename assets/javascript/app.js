37c3ee4aa5d0efe372ed8eb7571bf62cbc678b1e
Andrew Sirico <12siricoa94@gmail.com>
2019-09-19 20:51:18 -0400

    set up a few questions and started making game function with variables and a countdown function being prepared inside

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,12 @@ $("#start").on("click", function(){
 $(document).on("click", ".answer-button", function(e){
     game.clicked(e);
 });
+
+$(document).on("click", "#reset", function(){
+    game.reset();
+});
+
+var questions = [{
+    question: "When is it thought that the split between the hominin lineage from gorillas and chimpanzees occured?",
+    answers: ["8 million Years","12 million years","4 million years","Two days ago"]
+}]
